8c5bba92cfa8e2fb0e18e2df8b7e779ee12a3866
unknown <javier.rey.eu@gmail.com>
2026-06-07 15:42:49 +0100

    refactor cluster apps

--- packages/sys/index.js
+++ packages/sys/index.js
@@ -1,22 +1,39 @@
 // sys/index.js
 // _@ts-check
 
-import { globalState, hydrate } from 'jrjs-shared/packages/lib/sys/sys.js';
+/**
+@typedef {import('jrjs-shared/packages/lib/sys/run.js').PrimaryConfig} PrimaryConfig;
+*/
 
-const defaults = {
-  workerId: NaN,
-  processConfig: {
-    workersSize: 1, // 0, 1, 2, ... os.cpus().length
-    primaryApps: [],
-    workerApps: ['jrjs-shared/packages/lib/sys/server/run.js'],
-    ...JSON.parse(process.argv.slice(2).at(-1) || '{}'),
-  },
-  serverConfig: {
-    port: 3000,
-  },
+import { globalState, hydrate, log } from 'jrjs-shared/packages/lib/sys/sys.js';
+
+const params = JSON.parse(process.argv.slice(2).at(-1) || '{}');
+
+// import config from './index.json' with { type: 'json' };
+// const config = await import('./index.json', { assert: { type: 'json' } });
+// log.warn(`config !!!!!!`, JSON.stringify(config, null, 2)); process.exit(0);
+
+/** @type {PrimaryConfig} */
+const config = {
+  workersSize: 1, // 0, 1, 2, ... os.cpus().length
+  base: '',
+  apps: [
+    {
+      name: 'server',
+      path: 'jrjs-shared/packages/lib/sys/server/run.js',
+      primaryOnly: false,
+      requires: [],
+      state: {},
+      config: {
+        port: 3000,
+        publicDir: '../view',
+        privateDir: '../../_ignore/store',
+      },
+    },
+  ],
 };
 
-hydrate(globalState, defaults);
+hydrate(globalState.primaryConfig, params, config);
 
 // http://localhost:3000/Users/reyj/home/projects/apps/js/jrjs-template/packages/view
 // http://localhost:3000/Users/reyj/home/projects/apps/js/node-lab/www/plot-line-curve-svg/mathfun-svg/mathfun-svg.html
