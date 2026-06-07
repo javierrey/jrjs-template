22b21a2e2799c8353c6056e593bfb1eeb6a2faa8
unknown <javier.rey.eu@gmail.com>
2026-06-07 23:38:52 +0100

    use sysconfig

--- packages/sys/index.js
+++ packages/sys/index.js
@@ -2,10 +2,10 @@
 // _@ts-check
 
 /**
-@typedef {import('jrjs-shared/packages/lib/sys/run.js').PrimaryConfig} PrimaryConfig;
+@typedef {import('jrjs-shared/packages/lib/sys/sys.js').SysConfig} SysConfig;
 */
 
-import { globalState, hydrate, log } from 'jrjs-shared/packages/lib/sys/sys.js';
+import { sysConfig, hydrate, log } from 'jrjs-shared/packages/lib/sys/sys.js';
 
 const params = JSON.parse(process.argv.slice(2).at(-1) || '{}');
 
@@ -13,7 +13,7 @@ const params = JSON.parse(process.argv.slice(2).at(-1) || '{}');
 // const config = await import('./index.json', { assert: { type: 'json' } });
 // log.warn(`config !!!!!!`, JSON.stringify(config, null, 2)); process.exit(0);
 
-/** @type {PrimaryConfig} */
+/** @type {SysConfig} */
 const config = {
   workersSize: 1, // 0, 1, 2, ... os.cpus().length
   base: '',
@@ -33,7 +33,7 @@ const config = {
   ],
 };
 
-hydrate(globalState.primaryConfig, params, config);
+hydrate(sysConfig, params, config);
 
 // http://localhost:3000/Users/reyj/home/projects/apps/js/jrjs-template/packages/view
 // http://localhost:3000/Users/reyj/home/projects/apps/js/node-lab/www/plot-line-curve-svg/mathfun-svg/mathfun-svg.html
