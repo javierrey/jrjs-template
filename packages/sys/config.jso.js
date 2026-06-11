fa5ed7cc18ac57168e76a5b88853354cb6443ef7
unknown <javier.rey.eu@gmail.com>
2026-06-11 02:38:30 +0100

    config jso,types refactor

---
+++ packages/sys/config.jso.js
@@ -0,0 +1,31 @@
+// sys/config.jso.js
+// @ts-check
+
+/**
+@typedef {import('jrjs-shared/packages/lib/sys/sys.js').PlainObject} PlainObject;
+@typedef {import('jrjs-shared/packages/lib/sys/cluster.js').ClusterConfig} ClusterConfig;
+*/
+
+/** @type {PlainObject & ClusterConfig} */
+export default {
+  clusterSize: 1, // 0, 1, 2, ... os.cpus().length
+  base: '',
+  apps: [
+    {
+      name: 'server',
+      path: 'jrjs-shared/packages/lib/sys/server/run.js',
+      primary: false,
+      requires: [],
+      state: {},
+      config: {
+        port: 3000,
+        publicDir: '../view',
+        privateDir: '../../_ignore/store',
+      },
+    },
+  ],
+};
+
+// @remove test examples:
+// http://localhost:3000/Users/reyj/home/projects/apps/js/jrjs-template/packages/view
+// http://localhost:3000/Users/reyj/home/projects/apps/js/node-lab/www/plot-line-curve-svg/mathfun-svg/mathfun-svg.html
--- packages/sys/index.js
+++ packages/sys/index.js
@@ -1,40 +1,13 @@
 // sys/index.js
 // @ts-check
 
-/**
-@typedef {import('jrjs-shared/packages/lib/sys/sys.js').SysConfig} SysConfig;
-*/
-
-import { hydrate, sysConfig } from 'jrjs-shared/packages/lib/sys/sys.js';
+import { log, hydrate, sysConfig } from 'jrjs-shared/packages/lib/sys/sys.js';
+import config from './config.jso.js';
 
 const params = JSON.parse(process.argv.slice(2).at(-1) || '{}');
 
-// import config from './index.json' with { type: 'json' };
-// const config = await import('./index.json', { assert: { type: 'json' } });
-// log.warn(`config !!!!!!`, JSON.stringify(config, null, 2)); process.exit(0);
-
-/** @type {SysConfig} */
-const config = {
-  workersSize: 1, // 0, 1, 2, ... os.cpus().length
-  base: '',
-  apps: [
-    {
-      name: 'server',
-      path: 'jrjs-shared/packages/lib/sys/server/run.js',
-      primary: false,
-      requires: [],
-      state: {},
-      config: {
-        port: 3000,
-        publicDir: '../view',
-        privateDir: '../../_ignore/store',
-      },
-    },
-  ],
-};
-
 hydrate(sysConfig, params, config);
 
-// http://localhost:3000/Users/reyj/home/projects/apps/js/jrjs-template/packages/view
-// http://localhost:3000/Users/reyj/home/projects/apps/js/node-lab/www/plot-line-curve-svg/mathfun-svg/mathfun-svg.html
 import('jrjs-shared/packages/lib/sys/run.js');
+
+log.info(`sysConfig ${JSON.stringify(sysConfig, null, 2)}`);
