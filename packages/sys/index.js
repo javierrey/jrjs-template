88ffbb952040929a9f38933d78524b22f0e98939
Javier Rey <javier.rey.eu@gmail.com>
2026-06-18 16:22:29 +0100

    Update index.js

--- packages/sys/index.js
+++ packages/sys/index.js
@@ -2,7 +2,7 @@
 // @ts-check
 
 import { log, hydrate, jsonParse, sysConfig } from 'jrjs-shared/packages/lib/sys/sys.js';
-import config from './config.jso.js';
+import config from './config.obj.js';
 
 const params = jsonParse(process.argv.slice(2).at(-1) || '{}');
 
