564c5a1df05d7e8d967d930dcda6228a08165988
unknown <javier.rey.eu@gmail.com>
2026-08-27 01:24:35 +0100

    refactor load

--- packages/main/sys/config.js
+++ packages/main/sys/config.js
@@ -34,8 +34,7 @@ export default {
   ],
 };
 
-// @remove test examples:
-// http://localhost:3000
-// http://localhost:3000/content.html
+// @remove, test examples:
 // http://localhost:3000/mathfun
-// http://localhost:3000/service-one
+// http://localhost:3000/load.html?content=./content/document.md
+// http://localhost:3000/service-one?p1=v%201&p2=v%202
--- packages/main/sys/services/service-two.js
+++ packages/main/sys/services/service-two.js
@@ -1,11 +1,16 @@
 // main/sys/services/service-two
 
-import { log, toStr } from './shared.js';
+import { log, jsonStringify } from './shared.js';
 import serviceZero from './service-zero/index.js';
 
 const serviceTwo = (params) => {
   params ??= {}; params.name ||= 'serviceTwo';
-  const result = `<h1>serviceTwo</h1><pre>${toStr(serviceZero(params))}</pre>`;
+  const result = `
+    <link rel="stylesheet" href="./imported/lib/view/view.css"/>
+    <style>body { margin: 1rem; }</style>
+    <h1>serviceTwo</h1>
+    <pre>${jsonStringify(serviceZero(params), null, 2)}</pre>
+  `;
   return result;
 };
 
